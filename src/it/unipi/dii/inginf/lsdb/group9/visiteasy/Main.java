38816d66af52a97495d6673971e477c45bee5b40
Giuseppe <giuseppe.martino26@outlook.it>
2021-01-05 15:15:56 +0100

    .

--- src/it/unipi/dii/inginf/lsdb/group9/visiteasy/Main.java
+++ src/it/unipi/dii/inginf/lsdb/group9/visiteasy/Main.java
@@ -1,4 +1,28 @@
-import it.unipi.dii.inginf.lsdb.group9.visiteasy;
+package it.unipi.dii.inginf.lsdb.group9.visiteasy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import  java.util.*;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.println("-----sWelcome to VisitEasy----- \nSelect a command: \nI'm a:" +
+                    "\n1. Doctor" +
+                    "\n2. User" +
+                    "\n3. Administrator ");
+
+            int option = keyboard.nextInt();
+
+        }
+
+
+
+    }
 }
